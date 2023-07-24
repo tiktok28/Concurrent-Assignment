eb349d56ed9fe92beb9f5ca02ccddd324a8e4f46
Naveen Ramarao <naveen.gavara@growthops.asia>
2023-07-24 16:32:51 +0800

    Initial Release

--- src/Q2.java
+++ src/Q2.java
@@ -1,11 +1,23 @@
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import java.util.Iterator;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.ss.usermodel.Row;
-
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class Q2 {
+    public static void main(String[] args) {
+        try{
+            File file = new File("./18102673.xlsx");   //creating a new file instance
+            FileInputStream fis = new FileInputStream(file);   //obtaining bytes from the file
+            //creating Workbook instance that refers to .xlsx file
+            XSSFWorkbook wb = new XSSFWorkbook(fis);
+            XSSFSheet sheet = wb.getSheetAt(0);     //creating a Sheet object to retrieve object
+            Iterator<Row> itr = sheet.iterator();   //iterating over excel file
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
